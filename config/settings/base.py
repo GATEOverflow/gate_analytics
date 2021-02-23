6177cb13290501276043771ac18ae127c4db8e06
Pragy Agarwal <agar.pragy@gmail.com>
2021-02-24 04:41:50 +0530

    Revert "WIP: Initial draft for analytics"

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,7 +76,6 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "gate_analytics.users.apps.UsersConfig",
     # Your stuff: custom apps go here
-    "gate_analytics.analytics.apps.AnalyticsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- gate_analytics/analytics/algos/evaluate.py
+++
@@ -1,5 +0,0 @@
-from gate_analytics.analytics.models import AnswerKey, Submission, SubmissionEvaluation
-
-
-def evaluate(submission: Submission, key: AnswerKey) -> SubmissionEvaluation:
-    raise NotImplementedError()
